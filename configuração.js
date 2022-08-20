62243cdf600d97cc06c7b35f5deef7ce1cf010df
Caroline Quinteros <99746833+CarolineQuinteros@users.noreply.github.com>
2022-08-20 18:13:38 -0300

    Create configuração.js

---
+++ configuração.js
@@ -0,0 +1,16 @@
+$('.coruja-carrossel'). corujaCarousel({
+    loop:verdade,
+    margem:10,
+    nav:falso,
+    responsivo:{
+        0:{
+            itens:1
+        },
+        600:{
+            itens:3
+        },
+        1000:{
+            itens:5
+        }
+    }
+})
